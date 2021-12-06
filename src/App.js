8fd21a94b8f7c568b120573745345e7c9ef257ba
Egor <homer2734@gmail.com>
2021-12-06 04:14:34 +0300

    v(-1) WIP

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import 'reseter.css'
+import React, {useEffect, useState} from 'react'
+
+const handleLoad = () => {
+
+}
 
 function App() {
+    const BASE_URL = 'https://api.weatherbit.io/v2.0/forecast/daily';
+    const API_KEY = 'e4f2f55263ab4f9d82865b3d1dc24827';
+    const [latitude, setLatitude] = useState();
+    const [longitude, setLongitude] = useState();
+    const [city, setCity] = useState();
+    const [temps, setTemps] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const GetLocation = () => {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                setLatitude(position.coords.latitude);
+                setLongitude(position.coords.longitude);
+            });
+        } else {
+            console.log("Not Available");
+        }
+    }
+    const GetTemps = () => {
+        fetch(`${BASE_URL}?lat=${latitude}&lon=${longitude}&key=${API_KEY}`)
+            .then((res) => res.json())
+            .then((data) => {
+                do{
+                    let i = 0;
+                    setTemps(temps => [...temps, data.data[i].temp]);
+                    i++;
+                }
+                while(temps.length < 5);
+                setCity(data.city_name)
+                console.log(temps);
+                console.log(city);
+
+            })
+    }
+    useEffect(() => {
+        GetLocation();
+        GetTemps();
+    })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>
+          weather forecast
+        </h1>
       </header>
-    </div>
+      <section>
+          <div className="wrap">
+              <div className="card">
+                  <p>PLACEHOLDER</p>
+                  <p>PLACEHOLDER</p>
+                  <p>PLACEHOLDER</p>
+              </div>
+              <div className="card">
+                  <p>PLACEHOLDER</p>
+                  <p>PLACEHOLDER</p>
+                  <p>PLACEHOLDER</p>
+              </div>
+              <div className="card">
+                  <p>PLACEHOLDER</p>
+                  <p>PLACEHOLDER</p>
+                  <p>PLACEHOLDER</p>
+              </div>
+              <div className="card">
+                  <p>PLACEHOLDER</p>
+                  <p>PLACEHOLDER</p>
+                  <p>PLACEHOLDER</p>
+              </div>
+              <div className="card">
+                  <p>PLACEHOLDER</p>
+                  <p>PLACEHOLDER</p>
+                  <p>PLACEHOLDER</p>
+              </div>
+          </div>
+
+      </section>
+    </>
   );
 }
 
