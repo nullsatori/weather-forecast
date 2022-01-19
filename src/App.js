4876c5089411bb7afbd602d3e2753dad8b6cd613
Egor <homer2734@gmail.com>
2022-01-19 05:53:21 +0300

    v(-1) WIP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import 'reseter.css'
 import React, {useEffect, useState} from 'react'
-
-const handleLoad = () => {
-
-}
-
+import images from './index'
+const apl = images.a01d;
 function App() {
     const BASE_URL = 'https://api.weatherbit.io/v2.0/forecast/daily';
     const API_KEY = 'e4f2f55263ab4f9d82865b3d1dc24827';
@@ -12,36 +9,42 @@ function App() {
     const [longitude, setLongitude] = useState();
     const [city, setCity] = useState();
     const [temps, setTemps] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const [status, setStatus] = useState(0);
+    const [icons, setIcons] = useState([]);
+    let counter = 0;
     const GetLocation = () => {
-        if ("geolocation" in navigator) {
-            navigator.geolocation.getCurrentPosition(function(position) {
-                setLatitude(position.coords.latitude);
-                setLongitude(position.coords.longitude);
-            });
-        } else {
-            console.log("Not Available");
+        if(status === 0){
+            if ("geolocation" in navigator) {
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    setLatitude(position.coords.latitude);
+                    setLongitude(position.coords.longitude);
+                    setStatus(100);
+                    ;
+                });
+            } else {
+                console.log("Not Available");
+            }
         }
+        GetTemps()
     }
     const GetTemps = () => {
-        fetch(`${BASE_URL}?lat=${latitude}&lon=${longitude}&key=${API_KEY}`)
-            .then((res) => res.json())
-            .then((data) => {
-                do{
-                    let i = 0;
-                    setTemps(temps => [...temps, data.data[i].temp]);
-                    i++;
-                }
-                while(temps.length < 5);
-                setCity(data.city_name)
-                console.log(temps);
-                console.log(city);
-
-            })
+        if (status === 100){
+            fetch(`${BASE_URL}?lat=${latitude}&lon=${longitude}&key=${API_KEY}`)
+                .then((res) => res.json())
+                .then((data) => {
+                    while(counter < 5)
+                    {
+                        setTemps(temps => [...temps, data.data[counter].temp]);
+                        setIcons(icons => [...icons, data.data[counter].weather.icon])
+                        counter++;
+                    }
+                    setCity(data.city_name)
+                    setStatus(101);
+                })
+        }
     }
     useEffect(() => {
         GetLocation();
-        GetTemps();
     })
   return (
     <>
@@ -51,30 +54,26 @@ function App() {
         </h1>
       </header>
       <section>
+          <p id='cityName'>{city}</p>
           <div className="wrap">
               <div className="card">
-                  <p>PLACEHOLDER</p>
-                  <p>PLACEHOLDER</p>
+                  <img alt=""/>
+                  <p>{temps[0]}</p>
+              </div>
+              <div className="card">
+                  <p>{temps[1]}</p>
                   <p>PLACEHOLDER</p>
               </div>
               <div className="card">
-                  <p>PLACEHOLDER</p>
-                  <p>PLACEHOLDER</p>
-                  <p>PLACEHOLDER</p>
-              </div>
-              <div className="card">
-                  <p>PLACEHOLDER</p>
-                  <p>PLACEHOLDER</p>
+                  <p>{temps[2]}</p>
                   <p>PLACEHOLDER</p>
               </div>
               <div className="card">
-                  <p>PLACEHOLDER</p>
-                  <p>PLACEHOLDER</p>
+                  <p>{temps[3]}</p>
                   <p>PLACEHOLDER</p>
               </div>
               <div className="card">
-                  <p>PLACEHOLDER</p>
-                  <p>PLACEHOLDER</p>
+                  <p>{temps[4]}</p>
                   <p>PLACEHOLDER</p>
               </div>
           </div>
